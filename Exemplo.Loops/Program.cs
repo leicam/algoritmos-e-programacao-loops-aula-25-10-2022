702d94acd9e592e489a4dda4c332669040e6c042
Juliano Ribeiro de Souza Maciel <juliano.maciel@faculdadefacec.edu.br>
2022-10-25 20:22:00 -0300

    Implementações loops

---
+++ Exemplo.Loops/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Exemplo.Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExemploLoopFor();
+            Console.ReadKey();
+        }
+
+        private void ExemploLoopInfinito()
+        {
+            //loop infinito
+            while (true)
+            {
+                Console.WriteLine("PERDEU!");
+            }
+        }
+        private static void ExemploLoopWhile()
+        {
+            Console.WriteLine("Informe um valor para contagem" +
+                " regressiva?");
+            int valor = int.Parse(Console.ReadLine());
+
+            while (valor > 0)
+            {
+                valor = valor - 1;
+                Console.WriteLine(valor);
+            }
+        }
+
+        private static void ExemploLoopFor()
+        {
+            Console.WriteLine("Digite uma palavra para ser" +
+                "soletrada");
+            string palavra = Console.ReadLine();
+
+            for(int contador = 0; 
+                contador < palavra.Length; contador++)
+            {
+                Console.WriteLine(palavra[contador]);
+            }
+        }
+    }
+}
